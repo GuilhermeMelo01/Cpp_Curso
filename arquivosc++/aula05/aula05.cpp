0921abdf55547767cb7ae715356beab60af3d8af
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-07-20 18:48:43 -0300

    Aula 5 variaveis multiplas e constantes

---
+++ arquivosc++/aula05/aula05.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415 //Declaração de uma constante
+#define nome cout << "Guilherme Melo!\n\n"
+
+int main(){
+	
+	int vidas = 3, tiros = 500, life = 100;
+	
+	
+	cout << pi << "\n\n";
+	
+	nome;  
+
+	
+	return 0;
+}
